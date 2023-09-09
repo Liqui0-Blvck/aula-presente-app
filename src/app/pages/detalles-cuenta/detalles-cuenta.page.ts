8d8575d3ca151803c4f7379c6beaf6ea9feb7f28
AMARO HERRERA S <102757326+Liqui0-Blvck@users.noreply.github.com>
2023-09-09 07:11:31 -0300

    new features

--- src/app/pages/detalles-cuenta/detalles-cuenta.page.ts
+++ src/app/pages/detalles-cuenta/detalles-cuenta.page.ts
@@ -6,13 +6,19 @@ import { Subscription } from 'rxjs';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 import { FirestoreService } from 'src/app/services/firestore.service';
 import { User } from 'src/app/models';
+import { SharedService } from 'src/app/services/shared.service';
+
 @Component({
   selector: 'app-detalles-cuenta',
   templateUrl: './detalles-cuenta.page.html',
   styleUrls: ['./detalles-cuenta.page.scss'],
 })
 export class DetallesCuentaPage implements OnInit {
+  
   public uid = ''
+  isUpdating = false; 
+  userForm!: FormGroup
+  subscriberUserInfo!: Subscription;
 
   user: User = {
     uid: '',
@@ -30,26 +36,13 @@ export class DetallesCuentaPage implements OnInit {
     }
   };
 
-  userForm!: FormGroup
-
-  subscriberUserInfo!: Subscription;
-
   constructor(
     private formBuilder: FormBuilder,
     private loadingCtrl: LoadingController,
     private auth: AuthenticationService,
-    private router: Router,
+    private data: SharedService,
     private firebase: FirestoreService
-  ) { 
-    this.auth.stateAuth().subscribe((res) => {
-      if (res !== null) {
-        this.uid = res.uid;
-        this.getUserInfo(this.uid);
-      } else {
-        this.clearUserData();
-      }
-    });
-  }
+  ) { }
 
   ngOnInit() {
     this.userForm = this.formBuilder.group({
@@ -58,6 +51,20 @@ export class DetallesCuentaPage implements OnInit {
       direccion: [''],
       cuidad: [''],
       celular: ['']
+    })
+
+    this.data.getUser().subscribe((user) => {
+      if(user){
+        this.user = user;
+
+        this.userForm.patchValue({
+          nombre: this.user.datos_personales.nombre,
+          apellido: this.user.datos_personales.apellido,
+          direccion: this.user.datos_personales.direccion,
+          cuidad: this.user.datos_personales.cuidad,
+          celular: this.user.datos_personales.numero_celular
+        });
+      }
     })
   }
 
@@ -80,32 +87,10 @@ export class DetallesCuentaPage implements OnInit {
     };
   }
 
-  getUserInfo(uid: string) {
-    const path = 'users';
-    this.subscriberUserInfo = this.firebase.getDoc<User>(path, uid).subscribe((res) => {
-      if (res) {
-        this.user = res;
-        
-        this.userForm.patchValue({
-          nombre: this.user.datos_personales.nombre,
-          apellido: this.user.datos_personales.apellido,
-          direccion: this.user.datos_personales.direccion,
-          cuidad: this.user.datos_personales.cuidad,
-          celular: this.user.datos_personales.numero_celular
-        });
-
-      } else {
-        console.log('desconectado');
-      }
-    });
-  }
-
-  isUpdating = false; 
+  
 
   async updateUser() {
-
     if (this.isUpdating) {
-      // Si ya está en proceso de actualización, no hagas nada
       return;
     }
     const path = 'users';
@@ -114,27 +99,23 @@ export class DetallesCuentaPage implements OnInit {
     if (this.userForm.valid) {
       this.isUpdating = true;
 
-      // Obtén el documento actual del usuario
       const userData = this.user
-          // Extrae el campo "datos_personales" del documento actual
+          
           const datosPersonalesActuales = userData.datos_personales;
   
-          // Actualiza los campos dentro de "datos_personales" con los nuevos valores del formulario
+          
           datosPersonalesActuales.apellido = this.userForm.get('apellido')?.value;
           datosPersonalesActuales.nombre = this.userForm.get('nombre')?.value;
           datosPersonalesActuales.direccion = this.userForm.get('direccion')?.value;
           datosPersonalesActuales.cuidad = this.userForm.get('cuidad')?.value;
           datosPersonalesActuales.numero_celular = this.userForm.get('celular')?.value;
   
-          // Combina el objeto actualizado de "datos_personales" con el documento existente
+          
           const userToUpdate: User = {
             ...userData,
             datos_personales: datosPersonalesActuales
           };
-
-          console.log(userToUpdate)
-          // Actualiza el documento en Firestore
-
+        
             this.firebase.updateDoc(userToUpdate, path, userToUpdate.uid)
               .then(() => {
                 console.log('Datos personales actualizados con éxito');
@@ -143,8 +124,10 @@ export class DetallesCuentaPage implements OnInit {
               .catch((error) => {
                 console.error('Error al actualizar datos personales:', error);
               });
-        } else {
-          console.error('El documento del usuario no se encontró.');
         }
+
+    else {
+      console.error('El documento del usuario no se encontró.');
     }
+  }
 }
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -4,6 +4,8 @@ import { FirestoreService } from 'src/app/services/firestore.service';
 import { Carrera, Horarios, User } from 'src/app/models';
 import { Subscription } from 'rxjs';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Router } from '@angular/router';
+import { SharedService } from 'src/app/services/shared.service';
 
 
 
@@ -36,7 +38,7 @@ export class HomePage implements OnInit, OnDestroy {
 
   carrera: Carrera = {
     asignaturas: [],
-    nombre: '',
+    nombre: ''
   };
 
 
@@ -49,6 +51,8 @@ export class HomePage implements OnInit, OnDestroy {
   constructor(
     private auth: AuthenticationService,
     private firebase: FirestoreService,
+    private router: Router,
+    private data: SharedService
   ) {
     this.auth.stateAuth().subscribe((res) => {
       if (res !== null) {
@@ -94,16 +98,19 @@ export class HomePage implements OnInit, OnDestroy {
       this.subscriberUserInfo.unsubscribe();
     }
 
-    Camera.requestPermissions()
+    Camera.requestPermissions() 
+  
+  
   }
 
   getUserInfo(uid: string) {
     const path = 'users';
     this.subscriberUserInfo = this.firebase.getDoc<User>(path, uid).subscribe((res) => {
       if (res) {
-        console.log(res)
+
         this.user = res;
-        this.getInfoCarrera(this.user.carrera) 
+        this.data.setUser(this.user)
+
         if (!this.mostrarComponente) {
           this.getHorario(this.user.horario);
         }
@@ -113,23 +120,15 @@ export class HomePage implements OnInit, OnDestroy {
     });
   }
 
-  getInfoCarrera(carreraId: string) {
-    const path = 'carreras';
-    this.firebase.getDoc<Carrera>(path, carreraId).subscribe((res) => {
-      if (res) {
-        this.carrera.nombre = res.nombre;
-      } else {
-        console.log('La carrera no fue encontrada.');
-      }
-    });
-  }
 
   getHorario(horario_id: string){
   const path = 'horarios';
   this.firebase.getDoc<Horarios>(path, horario_id).subscribe(res => {
     if(res){
-      console.log(res)
       this.horarios = res;
+
+      this.data.setHorarios(this.horarios)
+
       const diaActual = new Date().toLocaleDateString('es-US', { weekday: 'long' }).toLowerCase();
       this.mostrarComponente = false; 
       this.componentes = [];
@@ -143,6 +142,7 @@ export class HomePage implements OnInit, OnDestroy {
         });
       });
     }
+
   });
 }
 
