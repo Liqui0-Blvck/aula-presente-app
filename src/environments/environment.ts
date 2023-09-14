bb6d61b02db345eab2df161c7ddddb052bebaa0f
Nicolas Cortes <102757326+Liqui0-Blvck@users.noreply.github.com>
2023-09-14 20:19:04 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,15 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyC7WVBZlXIGjVwgKTRaqSZNcHQdXkZeJDM",
-    authDomain: "aula-presente-116d7.firebaseapp.com",
-    projectId: "aula-presente-116d7",
-    storageBucket: "aula-presente-116d7.appspot.com",
-    messagingSenderId: "150193423946",
-    appId: "1:150193423946:web:41ae9ea47d4e0b50ad88b3"
-  }
+
 };
 
 /*
