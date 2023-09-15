b4e60915aea4e4679b902caeb8f8e525cb90dc49
AMARO HERRERA S <102757326+Liqui0-Blvck@users.noreply.github.com>
2023-09-14 21:27:26 -0300

    a

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-
+  firebaseConfig: {
+    apiKey: "AIzaSyC7WVBZlXIGjVwgKTRaqSZNcHQdXkZeJDM",
+    authDomain: "aula-presente-116d7.firebaseapp.com",
+    projectId: "aula-presente-116d7",
+    storageBucket: "aula-presente-116d7.appspot.com",
+    messagingSenderId: "150193423946",
+    appId: "1:150193423946:web:41ae9ea47d4e0b50ad88b3"
+  }
 };
 
 /*
